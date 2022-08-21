7531c874523e819943d4b2feda6043c2c7adab69
Veerocks <90183950+Veerocks@users.noreply.github.com>
2022-08-21 07:31:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function updateCanvas(){
+    background("white");
+}
+
+function draw(){
+    
+}
+
